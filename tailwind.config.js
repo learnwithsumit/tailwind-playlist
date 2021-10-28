ef0e801ed01b41f0d9119318308422b9b764c134
Sumit Saha <sumit@learnwithsumit.com>
2021-10-29 03:49:49 +0600

    lesson-6 code push

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {},
   },
@@ -8,4 +8,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
