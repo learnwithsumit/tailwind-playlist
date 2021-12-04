4d237e510bbb0c334a3bf7e95ac17095e665859a
Sumit Saha <sumit@learnwithsumit.com>
2021-12-05 03:26:35 +0600

    lesson 16

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,19 @@ module.exports = {
   purge: ["./public/**/*.{html,js}"],
   darkMode: "media", // or 'media' or 'class'
   theme: {
-    screens: {
-      sm: "540px",
-      lg: "1000px",
-      "4k": "2560px",
+    colors: {
+      blue: {
+        light: "#f5f5f5",
+        DEFAULT: "#f6f6f6",
+        dark: "#a5a5a5",
+      },
     },
     extend: {
+      screens: {
+        sm: "540px",
+        lg: "1000px",
+        "4k": "2560px",
+      },
       fontFamily: {
         sans: ["Roboto", ...defaultTheme.fontFamily.sans],
       },
