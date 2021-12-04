0926ba918837ae7e2f42bf7ff8e0241e014f26c5
Sumit Saha <sumit@learnwithsumit.com>
2021-12-05 03:19:02 +0600

    lesson 15

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   mode: "jit",
   purge: ["./public/**/*.{html,js}"],
   darkMode: "media", // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      sm: "540px",
+      lg: "1000px",
+      "4k": "2560px",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
